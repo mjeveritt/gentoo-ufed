1bdb819de722aba94ecbde8c56d7f14a4b76104d
Sven Eden <sven.eden@gmx.de>
2013-01-31 19:22:08 +0100

    Removed now obsolete type definitions as they are moved to ufed-curses-types.h. Further added some fucntion prototypes to draw various aspects of the display on demand.

--- ufed-curses.h
+++ ufed-curses.h
@@ -1,84 +1,32 @@
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
+#include "ufed-curses-types.h"
 
-#include <curses.h>
-
-
-/* debugging macros */
-#define DEBUG_EXIT 1
-#undef DEBUG_TRACE
-
-#if defined(DEBUG_EXIT)
-#  define ERROR_EXIT(code, fmt, ...) { \
-	cursesdone(); \
-	fprintf(stderr, "\nERROR in %s:%d (%s): \n -> ", \
-		__FILE__, __LINE__, __FUNCTION__); \
-	fprintf(stderr, fmt, __VA_ARGS__); \
-	exit(code); \
-}
-#else
-#  define ERROR_EXIT(code, ...) { cursesdone(); exit(code); }
-#endif // DEBUG_EXIT
-#if defined(DEBUG_TRACE)
-# define TRACE { \
-	fprintf(stderr, "(TRACE) %s:%d - %s\n", __FILE__, __LINE__, __FUNCTION__); \
-}
-#else
-# define TRACE
-#endif // DEBUG_TRACE
-
-
-/* global types */
-enum win { Top, Left, List, Input, Scrollbar, Right, Bottom, wCount };
-enum mask { show_unmasked, show_both, show_masked };
-enum order { pkgs_left, pkgs_right };
-enum scope { show_all, show_global, show_local };
-
-struct window {
-	WINDOW *win;
-	const int top, left, height, width;
-};
-
-struct item {
-	struct item *prev, *next;
-	int currline; //!< the current line on the screen this item starts.
-	int listline; //!< the fixed line within the full list this item starts
-	int ndescr;   //!< number of description lines
-	bool isMasked;
-	bool isGlobal;
-};
-
-struct key {
-	int key;
-	const char *descr;
-	size_t length;
-};
-
+/* global members */
+extern sWindow window[wCount];
 
 /* global prototypes */
 void cursesdone(void);
-int  getItemHeight(struct item *item);
 void initcurses(void);
-bool isLegalItem(struct item *item);
 
 int maineventloop(
 	const char *subtitle,
-	int (*callback)(struct item **currentitem, int key),
-	int (*drawitem)(struct item *item, bool highlight),
-	struct item *items,
-	const struct key *keys);
-void drawitems(void);
+	int (*callback)(sFlag** curr, int key),
+	int (*drawflag)(sFlag*  flag, bool highlight),
+	sFlag* flags,
+	const sKey* keys,
+	bool withSep);
+void drawBottom(bool withSep);
+void drawFlags(void);
+void drawStatus(bool withSep);
+void drawTop(bool withSep);
 bool scrollcurrent(void);
 bool yesno(const char *);
 
 
 /* global inline functions */
-extern struct window window[wCount];
-static inline WINDOW *win(enum win w) { return window[w].win; }
-static inline int wTop   (enum win w) { return (window[w].top   >= 0 ? 0 : LINES) + window[w].top   ; }
-static inline int wLeft  (enum win w) { return (window[w].left  >= 0 ? 0 : COLS ) + window[w].left  ; }
-static inline int wHeight(enum win w) { return (window[w].height > 0 ? 0 : LINES) + window[w].height; }
-static inline int wWidth (enum win w) { return (window[w].width  > 0 ? 0 : COLS ) + window[w].width ; }
+static inline WINDOW *win(eWin w) { return window[w].win; }
+static inline int wTop   (eWin w) { return (window[w].top   >= 0 ? 0 : LINES) + window[w].top   ; }
+static inline int wLeft  (eWin w) { return (window[w].left  >= 0 ? 0 : COLS ) + window[w].left  ; }
+static inline int wHeight(eWin w) { return (window[w].height > 0 ? 0 : LINES) + window[w].height; }
+static inline int wWidth (eWin w) { return (window[w].width  > 0 ? 0 : COLS ) + window[w].width ; }
 static inline int min(int a, int b) { return a < b ? a : b; }
 static inline int max(int a, int b) { return a > b ? a : b; }
